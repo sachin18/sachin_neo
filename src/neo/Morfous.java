8d4f097d4a6a430162fe8e07f46e44e0a9edd8f9
sachin18 <sachinsharma9424@gmail.com>
2017-06-30 14:35:24 +0530

    new changes with runni

--- src/neo/Morfous.java
+++ src/neo/Morfous.java
@@ -6,6 +6,9 @@ public class Morfous {
 		// TODO Auto-generated method stub
 System.out.println("hello perf");
 System.out.println("new perf");
+
+System.out.println("hello runni");
+System.out.println("new code");
 	}
 
 }
